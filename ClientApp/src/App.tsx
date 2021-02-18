9278de0901742e83f9786b8608d8a6deb4e71016
bmolnar91 <balintkilencvenegy@gmail.com>
2021-02-18 22:51:45 +0100

    feat(map): add a functioning MapWrapper

--- ClientApp/src/App.tsx
+++ ClientApp/src/App.tsx
@@ -1,10 +1,39 @@
-import React from "react";
-import { Dummy } from "./components/Dummy/Dummy";
+import React, { useState, useEffect } from "react";
+import GeoJSON from "ol/format/GeoJSON";
+import Feature from "ol/Feature";
+import Geometry from "ol/geom/Geometry";
+import "./App.css";
+import MapWrapper from "./components/map/MapWrapper";
 
 function App() {
+  // const [features, setFeatures] = useState<Feature<Geometry>[]>([]);
+
+  // useEffect(() => {
+  //   fetch("/mock-geojson-api.json")
+  //     .then((response) => response.json())
+  //     .then((fetchedFeatures) => {
+  //       const wktOptions = {
+  //         dataProjection: "EPSG:4326",
+  //         featureProjection: "EPSG:3857",
+  //       };
+  //       const parsedFeatures = new GeoJSON().readFeatures(
+  //         fetchedFeatures,
+  //         wktOptions
+  //       );
+
+  //       setFeatures(parsedFeatures);
+  //     });
+  // }, []);
+
   return (
-    <div>
-      <Dummy />
+    <div className="App">
+      <div className="app-label">
+        <p>React Functional Components with OpenLayers</p>
+        <p>Click the map to reveal location coordinate via React State</p>
+      </div>
+
+      {/* <MapWrapper features={features} /> */}
+      <MapWrapper features={[]} />
     </div>
   );
 }
--- ClientApp/src/components/map/MapWrapper.tsx
+++ ClientApp/src/components/map/MapWrapper.tsx
@@ -1,13 +1,87 @@
-import React, { useState, useRef } from "react";
+import React, { useState, useEffect, useRef } from "react";
+import Map from "ol/Map";
+import View from "ol/View";
+import TileLayer from "ol/layer/Tile";
+import VectorLayer from "ol/layer/Vector";
+import VectorSource from "ol/source/Vector";
+import XYZ from "ol/source/XYZ";
+import { transform } from "ol/proj";
+import { MapBrowserEvent } from "ol";
 
-const MapWrapper = (props: string) => {
-  const [map, setMap] = useState();
-  const [featuresLayer, setFeaturesLayer] = useState();
-  const [selectedCoord, setSelectedCoord] = useState();
+function MapWrapper(props: { features: any[] }) {
+  const [map, setMap] = useState<Map>();
+  const [featuresLayer, setFeaturesLayer] = useState<VectorLayer>();
+  const [selectedCoord, setSelectedCoord] = useState<number[]>();
 
-  const mapElement = useRef(null);
+  const mapElement = useRef<HTMLDivElement | null>(null);
+
+  const mapRef = useRef<Map | null | undefined>(null);
+  mapRef.current = map;
+
+  useEffect(() => {
+    const initalFeaturesLayer = new VectorLayer({
+      source: new VectorSource(),
+    });
+
+    const initialMap = new Map({
+      target: mapElement.current ?? undefined,
+      layers: [
+        new TileLayer({
+          source: new XYZ({
+            url: "http://mt0.google.com/vt/lyrs=p&hl=en&x={x}&y={y}&z={z}",
+          }),
+          visible: true,
+        }),
+
+        new TileLayer({
+          source: new XYZ({
+            url:
+              "https://basemap.nationalmap.gov/arcgis/rest/services/USGSTopo/MapServer/tile/{z}/{y}/{x}",
+          }),
+          visible: false,
+        }),
+
+        initalFeaturesLayer,
+      ],
+      view: new View({
+        projection: "EPSG:3857",
+        center: [0, 0],
+        zoom: 2,
+      }),
+      controls: [],
+    });
+
+    initialMap.on("click", handleMapClick);
+
+    setMap(initialMap);
+    setFeaturesLayer(initalFeaturesLayer);
+  }, []);
+
+  useEffect(() => {
+    if (props.features.length) {
+      featuresLayer?.setSource(
+        new VectorSource({
+          features: props.features,
+        })
+      );
+
+      map?.getView().fit(featuresLayer?.getSource().getExtent()!, {
+        padding: [100, 100, 100, 100],
+      });
+    }
+  }, [props.features]);
+
+  const handleMapClick = (event: MapBrowserEvent) => {
+    const clickedCoord = mapRef?.current?.getCoordinateFromPixel(event.pixel);
+
+    // TODO: make this work
+    const transformedCoord = transform(clickedCoord!, "EPSG:3857", "EPSG:4326");
+
+    setSelectedCoord(transformedCoord);
+    console.log(clickedCoord);
+  };
 
   return <div ref={mapElement} className="map-container"></div>;
-};
+}
 
 export default MapWrapper;
