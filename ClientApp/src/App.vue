4dc11e4042ac97fc0262f98f2febb5f47953ec54
bmolnar91 <balintkilencvenegy@gmail.com>
2021-03-10 18:24:27 +0100

    feat(client): make the App component listen to and render socket.io messages

--- ClientApp/src/App.vue
+++ ClientApp/src/App.vue
@@ -1,11 +1,28 @@
-<template></template>
+<template>
+  <div>
+    <p>{{ message }}</p>
+  </div>
+</template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { defineComponent, ref } from 'vue'
+import { io } from 'socket.io-client'
+
+const socket = io(`${process.env.VUE_APP_SERVER_DOMAIN}`)
 
 export default defineComponent({
-  name: 'App'
-});
+  name: 'App',
+  setup() {
+    const message = ref('')
+
+    socket.on('position message', (msg: string) => {
+      console.log(msg)
+      message.value = msg
+    })
+
+    return { message }
+  }
+})
 </script>
 
 <style>
