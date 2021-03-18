85c90ac4648a85339efe9c712222e44278059497
bmolnar91 <balintkilencvenegy@gmail.com>
2021-03-18 20:31:31 +0100

    feat(ui): install Vuetify via Vue CLI

--- ClientApp/src/main.ts
+++ ClientApp/src/main.ts
@@ -3,6 +3,7 @@ import App from './App.vue'
 import VueSocketIOExt from 'vue-socket.io-extended'
 import { io } from 'socket.io-client'
 import store from './store/index'
+import vuetify from './plugins/vuetify'
 
 const socket = io(
   `ws://${process.env.VUE_APP_SERVER_HOST}:${process.env.VUE_APP_SERVER_PORT}/`
@@ -14,5 +15,6 @@ Vue.use(VueSocketIOExt, socket, { store })
 
 new Vue({
   store,
+  vuetify,
   render: h => h(App)
 }).$mount('#app')
